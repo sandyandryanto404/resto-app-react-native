c7e4492a22dc3265dd89bad00c37a159f9af0c22
Sandy Andryanto <sandyandryanto@gmail.com>
2024-06-02 18:36:31 +0700

    up

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { FlatList, SafeAreaView, View, Text, StyleSheet, Image, TextInput, TouchableOpacity, ScrollView, Platform  } from "react-native";
+import { FlatList, SafeAreaView, StyleSheet  } from "react-native";
 import CardDasboard from "../components/CardDasboard"
 
 const DashboardScreen = () => {
-
-    const [visible, setVisible] = useState(false);
+    
     const [loading, setLoading] = useState(false);
     const [data, setData] = useState([]);
     const defaultData = [
